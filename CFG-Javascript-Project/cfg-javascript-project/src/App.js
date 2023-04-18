3b3f43e5585eebdac705dca2fd29ecb857c9661b
Jo-JoH-T <Jocelynht@outlook.com>
2023-04-18 19:03:38 +0100

    Adding a table to put Tiles in

--- CFG-Javascript-Project/cfg-javascript-project/src/App.js
+++ CFG-Javascript-Project/cfg-javascript-project/src/App.js
@@ -6,17 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <TileTest/>
+          <TileTest />
         </p>
+        <table>
+          <th>
+            <td><TileTest /></td>
+            <td><TileTest /></td>
+            <td><TileTest /></td>
+          </th>
+          <tr>
+            <td><TileTest /></td>
+            <td><TileTest /></td>
+            <td><TileTest /></td>
+          </tr>
+        </table>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
