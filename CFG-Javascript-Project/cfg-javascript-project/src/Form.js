6081f7ce3bed33246d5a0d7c0a5a076fef08348b
Shade-f <130912461+Shade-f@users.noreply.github.com>
2023-04-19 00:21:29 +0100

    Create Form.js
    Basic Registration form

---
+++ CFG-Javascript-Project/cfg-javascript-project/src/Form.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import './CSS'
+
+
+
+class Form extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            firstName: "",
+            lastName: "",
+            emailAddress: "",
+            gender: "",
+
+
+        }
+        this.handleSubmit=this.handleSubmit.bind(this)
+    }
+
+    firsthandler = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+    lasthandler = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+    emailhandler = (event) => {
+        this.setState({
+            emailAddress: event.target.value
+        })
+    }
+
+    genderhandler = (event) => {
+        this.setState({
+            gender: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert(`${this.state.firstName} ${this.state.lastName}  Registered Successfully!!!!`)
+        console.log(this.state);
+        this.setState({
+            firstName: "",
+            lastName: "",
+            emailAddress: '',
+            gender: "",
+        })
+     event.preventDefault()
+        
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <h1>Register For The CFG Festival 2023!!</h1>
+                    <label>FirstName :</label> <input type="text" value={this.state.firstName} onChange={this.firsthandler} placeholder="FirstName..." /><br />
+                    <label>LastName :</label> <input type="text" value={this.state.lastName} onChange={this.lasthandler} placeholder="LastName..." /><br />
+                    <label>EmailAddress :</label> <input type="text" value={this.state.emailAddress} onChange={this.emailhandler} placeholder="EmailAddress..." /><br />
+                    <label>Gender :</label><select onChange={this.genderhandler} defaultValue="Select Gender">
+                        <option defaultValue>Select Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select><br />
+                    <input type="submit" value="Submit" />
+                </form>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Form
